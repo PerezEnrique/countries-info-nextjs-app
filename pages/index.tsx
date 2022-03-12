f19e863504014f29a651cce021d5c1d621198718
Enrique Perez <28enriqueperez@gmail.com>
2022-03-12 12:08:25 -0400

    feat :heavy_plus_sign: Set getStaticProps on index to obtain countries data

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,30 @@
-import React from 'react'
+import React from 'react';
+import { getCountries } from '../services/countriesService';
+import { GetStaticProps, InferGetStaticPropsType } from 'next';
+import { Country } from '../countries.types';
 
-export default function index() {
-  return (
-    <div>index</div>
+export const getStaticProps: GetStaticProps<{countries: Country[], error?: string}> = async () => {
+    try {
+        return {
+            props: {
+                countries: await getCountries()
+            }
+        }
+    } catch (err) {
+        return {
+            props: {
+                countries: [],
+                error: "Sorry, something went wrong, please try refreshing the page later"
+            }
+        }
+    }
+}
+
+export default function index({countries, error}: InferGetStaticPropsType<typeof getStaticProps>) {
+  
+    return (
+    <div>
+        index
+    </div>
   )
 }
